3b5c7a1cd05f7806fe5afc39e728b705ed8714b3
Billy York <billyyork@Billys-Mac-mini.localdomain>
2025-02-17 21:19:09 -0600

    Add simple calculator

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello MSOE'
 
+@app.get('/calculate')
+def calculate():
+    op = request.args.get('operator')
+    arg1 = int(request.args.get('arg1'))
+    arg2 = int(request.args.get('arg2'))
+    if op == 'add':
+        return str(arg1 + arg2)
+    elif op == 'sub':
+        return str(arg1 - arg2)
+    elif op == 'mult':
+        return str(arg1 * arg2)
+    elif op == 'div':
+        return str(arg1 / arg2)
+    else:
+        return 'Not supported'
